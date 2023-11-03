37831e2b1a92ca2534a4751a3ac6f73dc274d710
furuyaJoel <joemon2112@gmail.com>
2023-11-03 16:03:24 +0900

    change scroll function before TypeScript project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Search from './components/Search';
 import unsplash from './api/unsplash';
 import ImgList from './components/ImgList';
+import './components/Search.css';
 
 function App(){
   const [images, setImages] = React.useState([]);
-  const onSearchSubmit = async(term) => {
-    
+  const [term, setTerm] = React.useState('');
+  const [page, setPage] = React.useState(1);
+
+  const onSearchSubmit = async(term, page) => {    
     const response = await unsplash.get('search/photos', {
-      params: { query: term}
+      params: { query: term, per_page: 21, page: page}
     });
-    setImages(response.data.results);
+     // pageが1だったらresponse.data.resultsを返す。1以外だったら、imagesに新しく取得したdataを結合する。
+    setImages( page === 1 ? response.data.results : images.concat(response.data.results));
   };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setPage(1);
+    onSearchSubmit(term, 1);
+  }
 
   return (
     <div className="App">
       <Header>
-        <Search onSearchSubmit={onSearchSubmit}/>
+        <form onSubmit={onFormSubmit}>
+          <input
+          type="text"
+          className="Search"
+          value={term}
+          placeholder="検索"
+          onChange={e => setTerm(e.target.value)}
+          />
+        </form>
       </Header>
       <div>
-        <ImgList images={images}/>
+        <ImgList images={images} onSearchSubmit={onSearchSubmit} term={term} pagge={page} setPage={setPage}/>
       </div>
     </div>
   );
--- src/components/ImgList.jsx
+++ src/components/ImgList.jsx
@@ -3,11 +3,29 @@ import ImgCard from './ImgCard'
 import './ImgList.css';
 
 const ImgList = (props) => {
+    // 画面をスクロールしたら画像をもう一度取得する関数
+    const handleScroll = (e) => {
+        const element = e.target;
+        const { scrollHeight, scrollTop, clientHeight } = element;
+        if (scrollHeight - scrollTop === clientHeight) {
+            console.log('reach end');
+            const newPage = props.page + 1;
+            props.setPage(newPage);
+            props.onSearchSubmit(props.term, newPage);
+        }
+    }
+
     const images = props.images.map((image) => {
         return <ImgCard key={image.Id} image={image} />;
     });
 
-    return <div className="img-list">{images}</div>;    
+    return (
+        <div 
+          onScroll={handleScroll}
+          style={{ height: '1000px', overflow: 'scroll' }}
+          className="image-list">{images}
+        </div>
+    );    
 };
 
 export default ImgList;
